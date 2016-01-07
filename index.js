3dc7b2532879d5747b8fd83277727be09c16542b
Brian Taber <btaber@diversecg.com>
2016-01-07 11:41:20 -0500

    -

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ module.exports = nodeEdgeOledb
 
 function nodeEdgeOledb(options, callback){
 	console.log("1")
-	if (!(this instanceof nodeEdgeOledb)) return new nodeEdgeOledb();
+	if (!(this instanceof nodeEdgeOledb)) return new nodeEdgeOledb(options, callback);
 	console.log("2")
 
 	adodb(options, function(error, result){
